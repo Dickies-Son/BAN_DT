8512afc1216259c96c71a2e8934c2939652ad6db
Dickies-Son <coqsjeunhan@gmail.com>
2023-09-25 14:39:46 +0700

    commit thử

---
+++ app/src/main/java/com/example/ban_dt/Xu_Ly_Giao_Dien/DrawerBaseActivity.java
@@ -0,0 +1,16 @@
+package com.example.ban_dt.Xu_Ly_Giao_Dien;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ban_dt.R;
+
+public class DrawerBaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer_base);
+    }
+}
